045e2af78fe9ca98dc0502b7d4fd8cb832c316ff
MaayanSal20 <Maayan.Salach@e.braude.ac.il>
2026-04-26 14:21:01 +0300

    Adding user management page

---
+++ t1/userManagement.js
@@ -0,0 +1,32 @@
+console.log("JS loaded");
+const currentUser=JSON.parse(localStorage.getItem('currentUser'));
+
+if (!currentUser){
+    alert("No user is logged in!")
+}
+
+const welcomeElement=document.getElementById('welcomeMessage');
+
+welcomeElement.textContent="Welcome "+currentUser.username;
+
+const users = JSON.parse(localStorage.getItem('users')) || [];
+const tableBody = document.getElementById('usersTableBody');
+
+users.forEach(user => {
+    const row=document.createElement('tr');
+    const usernameCell=document.createElement('td');
+    usernameCell.textContent=user.username;
+    const userEmailCell=document.createElement('td');
+    userEmailCell.textContent=user.email;
+    const userDobCell=document.createElement('td');
+    userDobCell.textContent=user.dob;
+    const userRoleCell=document.createElement('td');
+    userRoleCell.textContent=user.role;
+
+    row.appendChild(usernameCell);
+    row.appendChild(userEmailCell);
+    row.appendChild(userDobCell);
+    row.appendChild(userRoleCell);
+
+    tableBody.appendChild(row);
+});
